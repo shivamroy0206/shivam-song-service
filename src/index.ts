f156a127bb26aa9be631d9baa3e7b4b10552ff76
shivamroy0206 <shivamroy0206@gmail.com>
2025-07-22 00:52:50 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import doetnv from "dotenv";
+import songRoutes from "./route.js";
+import redis from "redis";
+import cors from "cors";
+
+doetnv.config();
+export const redisClient = redis.createClient({
+  password: process.env.Redis_Password,
+  socket: {
+    host: "redis-16072.crce206.ap-south-1-1.ec2.redns.redis-cloud.com",
+        port: 16072,
+  },
+});
+
+redisClient
+  .connect()
+  .then(() => console.log("connected to redis"))
+  .catch(console.error);
+
+
+
+const app = express();
+
+app.use(cors());
+
+app.use("/api/v1", songRoutes);
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
